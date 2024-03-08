80984633b569522cd58ad873484ac472a6601a01
Satyam Sharma <satyamshharish@gmail.com>
2024-03-08 17:50:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+  data:any = {};
+  Product:any = {
+    sale: 0,
+    revenue:0,
+    customer:0
+  }
+  
+  constructor(private socket: Socket){
+    
+  }
+
+  ngOnInit(): void {
+    this.data = this.Product
+    this.socket.fromEvent('new-email').subscribe((email:any) => {
+      console.log(email)
+    })
+
+    this.socket.fromEvent('server-resp').subscribe((resp:any) => {
+      console.log(resp)
+      this.data = resp.clientData;
+    })
+  }
+
+  sendMessage(){
+    console.log("clicked")
+    let clientData = {sender :"client", clientData : this.Product};
+    this.socket.emit("client-event", clientData);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
@@ -10,7 +20,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
